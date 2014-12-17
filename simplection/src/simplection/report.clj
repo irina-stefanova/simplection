30dae633ce0320cc5b82a10581c953aad945bba2
Irina Stefanova <irina.yaroslavova@gmail.com>
2014-12-18 00:14:29 +0200

    origin

---
+++ simplection/src/simplection/report.clj
@@ -0,0 +1,2 @@
+(ns simplection.report)
+
---
+++ simplection/test/simplection/core_test.clj
@@ -0,0 +1,7 @@
+(ns simplection.core-test
+  (:require [clojure.test :refer :all]
+            [simplection.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
