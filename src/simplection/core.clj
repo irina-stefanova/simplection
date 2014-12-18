b08e8d3cb2176e55ca6860423591a499a957159e
Irina Stefanova <irina.yaroslavova@gmail.com>
2014-12-19 01:13:32 +0200

    Include namespaces, initialize structure

--- src/simplection/core.clj
+++ src/simplection/core.clj
@@ -1,6 +1,22 @@
-(ns simplection.core)
+(ns simplection.core
+  (:require hiccup.core))
+
+(defn svg-quotes-helper
+  "In the REPL all double quotes are escaped. Therefore for quick copy-paste testing purposes use this helper to convert double quotes to single quotes"
+  [incoming-html]
+  (clojure.string/replace incoming-html "\"" "'"))
+
+(defn hiccup-to-svg
+  [svg]
+  (hiccup.core/html svg))
+
+(defn svg-element
+  "Writes the enclosing svg element used for all report items. Pass to it the whole rendered report."
+  [report]
+  [:svg {:width width :height height} report])
+  
 
 (defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  "Test svg."
+  []
+  [:svg {:width "400" :height "400"} [:path {:stroke "black" :D "m 150,100 l 300,300 z"}]])
