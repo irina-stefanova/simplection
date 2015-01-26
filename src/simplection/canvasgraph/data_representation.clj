0d4b1ef72dc144d45c87b4a11e16da62b68a9231
Irina <irina.yaroslavova@gmail.com>
2015-01-26 22:41:05 +0200

    Aggregates refactoring

--- src/simplection/canvasgraph/data_representation.clj
+++ src/simplection/canvasgraph/data_representation.clj
@@ -9,7 +9,7 @@
 (extend-protocol PDataRepresentation
   Category
   (generate-coordinates [{categories :coll coordinates-range :coordinates-range}] 
-    (let [categories-count (count categories)
+    (let [categories-count (count (distinct categories))
           categories-range [1 categories-count]
           offset-coordinates-range (simplection.utils/offset-range coordinates-range categories-count)]
       (simplection.utils/remap-values categories-range offset-coordinates-range (range 1 (inc categories-count))))))
